618938898a570eabdb38fae0ba2668aea1c2eca2
narupak <36034754+narupak@users.noreply.github.com>
2019-01-24 15:52:34 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xSm/7ijLL7twSNxbC+e5eVYtcgORsh8i5nB4C70tcgdGgYMauBH8UEFJHnsL0/YYYgyJa8WU8GgFomVtuk4id511qcUuQJfAhmBYoeb0PbUtjxuGtmRN8GE6J0z6XJYTcmf/sKjesm92ri1qrEWByQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
